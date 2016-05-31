7c781f7a69bca8c686b1b54ad8469abf6efe088b
Monika <reddy.moni88@gmail.com>
2016-05-31 12:52:48 +0100

    Commit 2

---
+++ src/main/java/com/collection/hi.java
@@ -0,0 +1,14 @@
+package com.collection;
+
+/**
+ * Created by akinom on 31/05/2016.
+ */
+public class hi {
+
+    public static void main(String[] args) {
+
+        System.out.println("hi");
+
+
+    }
+}
